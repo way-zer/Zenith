4de6eb42ae79dbeb3565e43d4120f2f409711f8a
way-zer <himc.wicp@gmail.com>
2021-05-15 23:19:15 +0800

    Update UI

--- src/ui/components/GameHub.ts
+++ src/ui/components/GameHub.ts
@@ -6,14 +6,13 @@ export class GameHub {
     ref = fgui.UIPackage.createObject('UI', 'GameHub').asCom
     score = this.ref.getChild('score').asLabel
     rankList = new RankList(this.ref.getChild('list').asList)
-    miniMap = this.ref.getChild('miniMap').asLoader
-    createBtn = this.ref.getChild('createUnit').asButton
+    // miniMap = this.ref.getChild('miniMap').asLoader
+    // createBtn = this.ref.getChild('createUnit').asButton
 
     constructor(private ui: TheUI) {
         this.ref.visible = false
         GRoot.inst.addChild(this.ref)
-        this.miniMap.visible = false
-        this.createBtn.addClickListener(this.createUnit, this)
+        // this.createBtn.addClickListener(this.createUnit, this)
     }
 
     createUnit() {
